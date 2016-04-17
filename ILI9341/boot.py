bc9711a21f1f31b57ca9e6af49819451b7119535
ropod7 <ropod7@gmail.com>
2016-04-18 01:55:22 +0300

    remove main.py boot.py

--- ILI9341/boot.py
+++
@@ -1,8 +0,0 @@
-# boot.py -- run on boot-up
-# can run arbitrary Python, but best to keep it minimal
-
-import machine
-import pyb
-pyb.main('main.py') # main script to run after this one
-pyb.usb_mode('CDC+MSC') # act as a serial and a storage device
-#pyb.usb_mode('CDC+HID') # act as a serial device and
