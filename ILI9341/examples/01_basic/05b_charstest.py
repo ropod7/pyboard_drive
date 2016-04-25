419690cadcfdf656b0ede523fcd763d8900edd6f
Roman Podgaiski <ropod7@gmail.com>
2016-04-25 12:30:22 +0300

    Update 05b_charstest.py

--- ILI9341/examples/01_basic/05b_charstest.py
+++ ILI9341/examples/01_basic/05b_charstest.py
@@ -7,9 +7,6 @@ from lcd import *
 l = LCD( rate=21000000 ) # step down the SPI bus speed to 21 MHz may be opportune when using 150+ mm wires
 
 # Draw characters in black on white background for CONFORTABLE READING.
-#
-# For more details about drawing internals, please see the 05c_charstest.py sample
 
 l.fillMonocolor( WHITE )
-l.charsTest( color=BLACK, bgcolor=WHITE, font=Arial_14 )
-
+l.charsTest( color=BLACK, font='Arial_14' )
